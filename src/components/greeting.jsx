5905a21a80a482557d5e0e7da236567970fdea73
Chethana45 <chethanasriadusumalli@gmail.com>
2026-09-23 23:16:16 -0700

    Create greeting.jsx

---
+++ src/components/greeting.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Greeting({ name }) {
+  return (
+    <div className="card">
+      <h2>Hello, {Chethana}! 👋</h2>
+      <p>Welcome to my React practice project.</p>
+    </div>
+  );
+}
+
+export default Greeting;
