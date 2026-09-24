2886fb9065da148e06674b06fe468cd8c3861662
Chethana45 <chethanasriadusumalli@gmail.com>
2026-09-23 23:17:01 -0700

    Create usercard.jsx

---
+++ src/components/usercard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserCard({ name, role }) {
+  return (
+    <div className="card">
+      <h2>👩‍💻 {Chethana}</h2>
+      <p>{role}</p>
+      <p>Learning React and building projects.</p>
+    </div>
+  );
+}
+
+export default UserCard;
